fba8cb9875b8722c5ec192c0d8ba93f23db9c590
Nattachai12 <nattachainudol@gmail.com>
2022-03-29 20:56:33 -0700

    add comment

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App.jsx";
 import styles from "./styles/app.css";
-//s
+//ssda
 render(
   <BrowserRouter>
     <App />
