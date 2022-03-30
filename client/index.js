43b1d80a50cf0f97245a2558718d1de99965a910
Nattachai12 <nattachainudol@gmail.com>
2022-03-29 20:41:32 -0700

    hello

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App.jsx";
 import styles from "./styles/app.css";
-
+//s
 render(
   <BrowserRouter>
     <App />
