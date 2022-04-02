4aea6f540caef7e161558b00b7ee212a30a9059b
mfiloramo <mlfiloramo@gmail.com>
2022-04-02 16:01:35 -0400

    continued setting up client chatbox

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Chatbox from "./components/Chatbox";
 import Signup from './pages/signup'
 import Login from './pages/login'
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss'
 
-const messages = [];
 
 /** MAIN APP LIVES HERE */
 function App() {
 
-  <div className="App">
-      <Routes>
-          <Route path='/' element={<Login/>} />
-          <Route path='/signup' element={<Signup/>} />
-      </Routes>
-  </div>
+  return (
+    <div className="App">
+      <Chatbox />
+        <Routes>
+            <Route path='/' element={<Login/>} />
+            <Route path='/signup' element={<Signup/>} />
+        </Routes>
+    </div>
+  )
 }
 
 export default App;
