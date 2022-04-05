ce7beb8913760e0d814c7bc99b93148d1d5a5af3
Mikel Giffin <giffinmike@hotmail.com>
2022-04-04 17:51:43 -0700

    signup page functionality done

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ReactDOM from "react-dom";
+import axios from 'axios'
+import click1 from '../audioclips/click1.mp3';
 
 function Login({ userInput, props }) {
   const handleChange = (e) => {
@@ -14,16 +16,20 @@ function Login({ userInput, props }) {
   }
   return (
     <div>
-      <Link to="/signup">hello</Link>
-      <input
-        name="email"
-        type="text"
-        placeholder="email"
-        onChange={handleChange}
-      />
+        <div className="photo">
+        <div className="fade-in-image1">
+          <h1>Dev-Helpr</h1>
+        </div>
+      {/* <Link to="/signup">hello</Link> */}
+
+      <input name="email" className ="emailField" type="text" placeholder="email" onChange={handleChange}/>
+
       <input name="password" type="password" onChange={handleChange} />
+
       <button type="submit" onClick={handleClick}>Submit</button>
-      {/*</form>*/}
+
+    
+    </div>
     </div>
   );
 }
