dcd8a4654cf7d85acb863e93dacf39888f82b0c2
Nattachai12 <nattachainudol@gmail.com>
2022-04-05 19:28:44 -0700

    update popup

--- client/component/ticketCreator.js
+++ client/component/ticketCreator.js
@@ -3,12 +3,14 @@ import second from "./../stylesheets/ticketCreator.css";
 
 function TicketCreator() {
   const handleSubmit = () => {
-    
+
   }
   return (
     <div className="ticketCreator">
       <div className="ticketCreator__content">
-        <form onSubmit={handleSubmit}></form>
+        <form onSubmit={handleSubmit}>
+          ushdfouhoiefh
+        </form>
       </div>
     </div>
   );
