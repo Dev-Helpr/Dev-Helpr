59359af6313bb07bec90d7801db6b75372a57e61
Nattachai12 <nattachainudol@gmail.com>
2022-04-11 15:55:00 -0700

    Add more comments for other people to understand the logic better

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Providing a UI for the dev. Community where members can log in and ask for help or help other developers
+
+
--- client/pages/home.js
+++ client/pages/home.js
@@ -23,21 +23,25 @@ function Home({
   getTicketStateWhenClickEdit,
 }) {
   ;
-  
+  //if false, it's an empty div. If true, it will show the whole ticket in the middle of the screen
   const [ticketIsClick, setTicketIsClick] = useState(false);
   const [createTicketIsClick, setCreateTicketIsClick] = useState(false);
+  //Used in useEffect to get all the tickets from the database
   const [arrOfTicket, setArrOfTicket] = useState([]);
   const [arrOfUsers, setArrOfUsers] = useState([]);
+  //User can click their username on the right siide to change status from online, to helper, to seeking help
   const [updateStatus, setUpdateStatus] = useState(true);
+  //This will grab all data from the ticket that is clicked and props drill into TicketDescription.js to show the whoe ticket in the middle of the screen
   const [ticketDisplay, setTicketDisplay] = useState({});
   const clickAudio = () => new Audio(click1).play();
   const filterOnlineUserNames = (userFromList) => {
     if (userFromList.props.online === true) {
-      return true 
-    } 
+      return true
+    }
     return false
   };
   const handleClick = (id) => {
+    //config is used
     const config = {
       headers: {
         Authorization: `Bearer ${user.accessToken}`,
@@ -51,9 +55,11 @@ function Home({
       })
       .catch((err) => console.log(err));
   };
-  //this whole function
-  //fetch for users
+
+  //fetch all users from DB. This function is called inside useEffect
   const fetchUserList = () => {
+    //Every call to the database will need accessToken otherwise the request will not go through.
+    //accessToken has a lifepan of 10 mins and is stored inside user state.
     const config = {
       headers: {
         Authorization: `Bearer ${user.accessToken}`,
@@ -62,8 +68,7 @@ function Home({
     axios
       .get("/api/users/", config)
       .then((res) => {
-        //expected this to be an array of object, if not ticket then it will be an empty array
-        // console.log("USER LIST:  ", res.data);
+        //res.data is an array of users' object
         const array = [];
         // setArrOfTicket(res.data);
         for (let i = 0; i < res.data.length; i++) {
@@ -83,6 +88,7 @@ function Home({
       })
       .catch((err) => console.log(err));
   };
+
   useEffect(() => {
     //fetch for tickets
     const config = {
@@ -115,7 +121,7 @@ function Home({
     fetchUserList();
   }, [arrOfTicket.length, updateStatus]);
 
-  
+
   return (
     <div className="home">
       <img className="signupPhoto" src={signupBackground} />
@@ -125,9 +131,9 @@ function Home({
         <box className="ticketBody"></box>
         <box className="usersList-Box1"></box>
         <box className="chatBox">
-          <Chatbox 
+          <Chatbox
           // socket={socket}
-           user={user} 
+           user={user}
            onlineUsers={arrOfUsers.filter(filterOnlineUserNames)}/>
         </box>
         <box className="usersContainer-box">
@@ -169,6 +175,7 @@ function Home({
       ) : (
         <div className="ticketDescription"></div>
       )}
+      {/* Too much prop drills. Kind of kill the purpose of using redux. */}
       {createTicketIsClick ? (
         <TicketCreator
           user={user}
@@ -181,7 +188,6 @@ function Home({
           user_id={user.id}
         />
       ) : null}
-      {/* userFeed */}
       <div className="switchContainer">
         <input type="checkbox" id="switch" name="switch" />
         <label
